8c25f5e4b0370e7df58091a54bac9b2c7d79bc3e
Jonathan Pauda <JonathanPauda@JonathanPs-MBP.app.byu.edu>
2018-02-01 11:54:38 -0700

    jhgfd

--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    question_text = models.TextField(blank=True, null=True)
+    pub_date = models.DateTimeField(blank=True, null=True)
+    duration = models.IntegerField(null=True)
+
+
+
+# class Choice(models.Model):
+#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+#     choice_text = models.CharField(max_length=200)
+#     votes = models.IntegerField(default=0)

f35f8c82324de15564aca5f3a6a5d356d81d1acb
Jonathan Pauda <JonathanPauda@JonathanPs-MBP.app.byu.edu>
2018-03-06 17:20:56 -0700

    finished sprint 3

--- account/views/logout.py
+++ account/views/logout.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate
+from django.contrib.auth import logout
 from django.http import HttpResponseRedirect
 from django_mako_plus import view_function
 from django import forms
@@ -7,7 +7,7 @@ from django import forms
 def process_request(request):
     #log the user out
     logout(request)
-    return HttpResponseRedirect('/account/')
+    return HttpResponseRedirect('/homepage/')
 
 
 
--- account/views/signup.py
+++ account/views/signup.py
@@ -2,6 +2,10 @@ from django.http import HttpResponseRedirect
 from django_mako_plus import view_function
 from django import forms
 from formlib import Formless
+from account import models as amod
+import re
+from django.contrib.auth import authenticate, login
+
 
 @view_function
 def process_request(request):
@@ -13,7 +17,7 @@ def process_request(request):
     if form.is_valid():
         form.commit()
         #work of the form- create user, login user, purcahase
-        return HttpResponseRedirect('account/')
+        return HttpResponseRedirect('/account/index')
 
 
     # render the form
@@ -29,24 +33,36 @@ class TestForm(Formless):
         self.fields['password'] = forms.CharField(label= 'Password', widget=forms.PasswordInput())
         self.fields['password2'] = forms.CharField(label= 'Again Please', widget=forms.PasswordInput())
 
+    def clean_email(self):
+        e1 = self.cleaned_data.get('email')
+        if amod.User.objects.filter(email = e1).count() > 0:
+            raise forms.ValidationError('Email already exists, please login')
+        return e1
 
-    def clean_age(self):
-        age = self.cleaned_data.get('age')
-        if age <18:
-            raise forms.ValidationError('you are not 18, no soup for you')
-        return 1010
+    def clean_password(self):
+        p1 = self.cleaned_data.get('password')
+        if len(p1) < 8:
+            raise forms.ValidationError('Please enter a password with 8 or more characters')
+        if not re.search('\d', p1):
+            raise forms.ValidationError('Please enter a password with a Number')
+        return p1
 
     def clean(self):
         p1 = self.cleaned_data.get('password')
         p2 = self.cleaned_data.get('password2')
 
-        if len(p1) < 8:
-            raise forms.ValidationError('Please enter a password with 8 or more characters')
         if p1 != p2:
             raise forms.ValidationError('Please ensure the passwords match.')
         return self.cleaned_data
+    def commit(self):
+        user = amod.User()
+        user.email = self.cleaned_data.get('email')
+        user.set_password(self.cleaned_data.get('password'))
+        user.save()
+        user = authenticate(email=self.cleaned_data.get('email'), password=self.cleaned_data.get('password'))
+        login(self.request, user)
 
-    #email unique
+
 
 
 
